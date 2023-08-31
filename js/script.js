a08ce8a079bb665bbec1cf4a04699e7dbe2d810e
Andrey Rubtsov <aa.rubtsov@icloud.com>
2023-09-01 00:13:17 +0300

    correction #01.09.2023

--- js/script.js
+++ js/script.js
@@ -1,28 +1,53 @@
-let hour = 1;
-let minutes = 5;
-let seconds = 5;
+// let hour = 2;
+// let minutes = 0;
+// let seconds = 0;
+
+let element = document.querySelector('.container');
+
+let startValueCountdown = {
+    hour: 0,
+    minutes: 0,
+    seconds: 0,
+};
+
+let countdown;
+
+function timer() {
+
+    clearInterval(countdown);
+
+    countdown = setInterval(() => {
+
+        console.log('timer =', `${this.hour}:${this.minutes < 10 ? '0' : ''}${this.minutes}:${this.seconds < 10 ? '0' : ''}${this.seconds}`);
+
+        if (this.hour === 0 && this.minutes === 0 && this.seconds === 0) {
+            clearInterval(countdown);
+        }
+
+        if (this.seconds === 0) {
+            this.minutes == 0 ? 0 : this.minutes--;
+            this.seconds = 60;
+        }
+
+        this.seconds--;
+
+        if (this.hour > 0 && this.minutes === 0) {
+            this.hour--;
+            this.minutes = 59;
+        }
+
+    }, 250)
+}
 
 
-let countdown = setInterval(() => {
+// timer.call(startValueCountdown);
 
-    seconds--;
+element.addEventListener('click', (event) => {
+    if (event.target.dataset.num == '1') {
+        startValueCountdown.minutes = parseInt(document.querySelector('#minutes').value);
 
-    console.log('hour=', hour);
-    console.log('minutes=', minutes);
-    console.log('seconds=', seconds);
-
-    if (hour === 0 && minutes === 0 && seconds === 0) {
-        clearInterval(countdown);
-    }
-
-    if (seconds === 0) {
-        minutes--;
-        seconds = 10;
-    }
-
-    if (hour > 0 && minutes === 0) {
-        hour --;
-        minutes = 5;
-    }
-
-}, 1000)
+        timer.call(startValueCountdown);
+        console.log(startValueCountdown)
+    };
+    // console.log(event.target.dataset.num == '1');
+});
